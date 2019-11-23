0eef3e50ad8c0d155b56cd97d9026b18bb227a21
jphee <jphee@uwo.ca>
2019-11-23 16:02:07 -0500

    New client that works

--- client.py
+++ client.py
@@ -1,34 +1,63 @@
-import socket 
-import select 
-import sys 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#input server ip
-#print("Enter server IP address: ")
-#IP = str(input()) 
-IP='127.0.0.1'
+"""Script for Tkinter GUI chat client."""
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+from tkinter import *
 
-#input port number for the server
-#print("Enter server port number: ")
-#port = int(input())
-port=3000
 
-#connects to server using IP and Port
-server.connect((IP, port))
+def receive():
+    """Handles receiving of messages."""
+    while True:
+        try:
+            recvMessage = client_socket.recv(4096).decode("utf8")
+            msg_list.insert(END, recvMessage)
+        except OSError:  # Possibly client has left the chat.
+            break
 
-while True: 
-# maintains a list of possible input streams 
-    sockets_list = [sys.stdin, server] 
-#below code: if retrieving from server, go to "if" statement
-#if sending to server go to "else" statement
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
-    for socks in read_sockets: 
-        if socks == server: 
-            message = socks.recv(2048).decode() 
-            print (message) 
-        else:
-           
-            message = str(input()) 
-            server.send(message.encode())
-            sys.stdout.write("\n<You>") 
-            sys.stdout.flush() 
-server.close() 
+
+def send(event=None):  # event is passed by binders.
+    """Handles sending of messages."""
+    msg = my_msg.get()
+    my_msg.set("")  # Clears input field.
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        root.quit()
+
+
+def on_closing(event=None):
+    """This function is to be called when the window is closed."""
+    my_msg.set("{quit}")
+    send()
+
+root = Tk()
+root.title("Chat Client Connection")
+
+messages_frame = Frame(root)
+my_msg = StringVar()  # For the messages to be sent.
+my_msg.set("Type your messages here.")
+scrollbar = Scrollbar(messages_frame)  # To navigate through past messages.
+# Following will contain the messages.
+msg_list = Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+scrollbar.pack(side=RIGHT, fill=Y)
+msg_list.pack(side=LEFT, fill=BOTH)
+msg_list.pack()
+messages_frame.pack()
+
+entry_field = Entry(root, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack()
+send_button = Button(root, text="Send", command=send)
+send_button.pack()
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+#----Now comes the sockets part----
+
+ADDR = ("127.0.0.1", 3000)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDR)
+
+receive_thread = Thread(target=receive)
+receive_thread.start()
+root.mainloop()  # Starts GUI execution.
