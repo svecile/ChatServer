a016b6f4b16572aa29405b01e257b7c0f9086e46
jphee <jphee@uwo.ca>
2019-11-29 11:50:41 -0500

    Final client

--- client.py
+++ client.py
@@ -14,21 +14,21 @@ def connect():
             """This function is to be called when the window is closed."""
             my_msg.set("dsnfjnd")
             send()
-            flag = 1
             client_socket.close()
         chatWindow.destroy()
 
     def receive():
         """Handles receiving of messages."""
         while True:
-            if flag:
-                break
             try:
                 recvMessage = client_socket.recv(4096).decode("utf8")
+                if recvMessage == "dsnfjnd":
+                    break
                 msg_list.insert(END, recvMessage)
                 msg_list.yview_moveto(1)
-            except OSError:  # Possibly client has left the chat.
+            except OSError:  # Client may have left the chat and handles closing the thread when the chat window exits
                 break
+        print("Test")
 
     def send(event=None):  # event is passed by binders.
 
@@ -41,10 +41,10 @@ def connect():
             updateSendText.set("Message not sent. Connection may have lost")
             reconnectButton.config(state="normal")
 
-    flag = 0
     sendToServer = 1
     chatWindow = Toplevel(root)
     chatWindow.title("Chat Away!")
+    chatWindow.grab_set()
     width = 800
     height = 400
     chatWindow.minsize(width=width, height=height)
@@ -61,23 +61,25 @@ def connect():
     my_msg = StringVar()  # For the messages to be sent.
     scrollbar = Scrollbar(messages_frame)  # To navigate through past messages.
     # Following will contain the messages.
-    msg_list = Listbox(messages_frame, height=15, width=80,
-                       yscrollcommand=scrollbar.set)
+    msg_list = Listbox(messages_frame, height=15, width=120, yscrollcommand=scrollbar.set)
     scrollbar.pack(side=RIGHT, fill=Y)
     msg_list.pack(side=LEFT, fill=BOTH)
     msg_list.pack()
     messages_frame.pack()
-    entry_field = Entry(chatWindow, textvariable=my_msg, width = 65)
-    entry_field.bind("<Return>", send)
-    entry_field.pack(pady=5)
-    send_button = Button(chatWindow, text="Send", command=send)
-    send_button.pack()
 
-    reconnectButton = Button(chatWindow, text="Reconnect To Server", command=reconnect, fg="green", state=DISABLED)
-    reconnectButton.pack()
+    inputFrame = Frame(chatWindow)
+    inputFrame.pack(pady=5)
+    entry_field = Entry(inputFrame, textvariable=my_msg, width = 65)
+    entry_field.bind("<Return>", send)
+    entry_field.grid(column=0, row=0, padx=5)
+    send_button = Button(inputFrame, text="Send", command=send, relief=GROOVE)
+    send_button.grid(column=1, row=0)
+
+    reconnectButton = Button(chatWindow, text="Reconnect To Server", command=reconnect, fg="green", state=DISABLED, relief=GROOVE)
+    reconnectButton.pack(pady=20)
 
     chatWindow.protocol("WM_DELETE_WINDOW", on_closing)
-    ADDR = ("54.162.255.141", 3000)
+    ADDR = ("3.83.189.76", 3000)
 
     client_socket = socket(AF_INET, SOCK_STREAM)
     try:
@@ -91,6 +93,8 @@ def connect():
         reconnectButton.config(state = "normal")
     receive_thread = Thread(target=receive)
     receive_thread.start()
+
+    print("DOne")
    
 
 
@@ -105,48 +109,15 @@ width = 500
 height = 400
 root.minsize(width=width, height=height)
 
-containingFrame = Frame(root)
-containingFrame.pack(pady=30)
+welcomeFrame = Frame(root)
+welcomeLabel = Label(welcomeFrame, text="Welcome to the chat app!")
+welcomeLabel.pack()
+welcomeFrame.pack(pady=20)
 
-ipFrame = Frame(root)
-ipFrame.pack(pady=10)
-# input server ip
-ipLabel = Label(ipFrame, text="Enter server IP address: ")
-ipLabel.config(font=("Helvetica", 12))
-ipLabel.pack()
-ip_entry = StringVar()
-ip_entry_field = Entry(ipFrame, textvariable=ip_entry, width="60")
-ip_entry_field.pack()
-
-# print("Enter server IP address: ")
-# IP = str(input())
-
-portFrame = Frame(root)
-portFrame.pack(pady=10)
-# input port number for the server
-portLabel = Label(portFrame, text="Enter server port number: ")
-portLabel.config(font=("Helvetica", 12))
-portLabel.pack()
-port_entry = StringVar()
-port_entry_field = Entry(portFrame, textvariable=port_entry, width="60")
-port_entry_field.pack()
-
-nameFrame = Frame(root)
-nameFrame.pack(pady=10)
-# input for user's name
-nameLabel = Label(nameFrame, text="Enter your name: ")
-nameLabel.config(font=("Helvetica", 12))
-nameLabel.pack()
-name_entry = StringVar()
-name_entry_field = Entry(nameFrame, textvariable=name_entry, width="60")
-name_entry_field.pack()
-
-connect_button = Button(root, text="Connect", command=connect,
-                        relief=GROOVE, width="15", height="1", fg="green")
+connect_button = Button(root, text="Connect", command=connect, relief=GROOVE, width="15", height="1", fg="green")
 connect_button.pack(pady=10)
 
-exit_button = Button(root, text="Exit", command=closeMain,
-                     relief=GROOVE, width="15", height="1", fg="red")
+exit_button = Button(root, text="Exit", command=closeMain, relief=GROOVE, width="15", height="1", fg="red")
 exit_button.pack(pady=10)
 
 
