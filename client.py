b48223d0739b71cb23a46afd7a7a5d629e8d946c
Spencer <svecile@uwo.ca>
2020-03-04 14:54:44 -0500

    updated server with complete functionality

--- client.py
+++ client.py
@@ -1,30 +1,136 @@
-import socket 
-import select 
-import sys 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#input server ip
-print("Enter server IP address: ")
-IP = str(input()) 
-#input port number for the server
-print("Enter server port number: ")
-port = int(input())
-#connects to server using IP and Port
-server.connect((IP, port))
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+from tkinter import *
+import time
 
-while True: 
-# maintains a list of possible input streams 
-    sockets_list = [sys.stdin, server] 
-#below code: if retrieving from server, go to "if" statement
-#if sending to server go to "else" statement
-    read_sockets,write_socket, error_socket = select.select(sockets_list,[],[]) 
-    for socks in read_sockets: 
-        if socks == server: 
-            message = socks.recv(2048).decode() 
-            print (message) 
-        else:
-           
-            message = str(input()) 
-            server.send(message.encode())
-            sys.stdout.write("\n<You>") 
-            sys.stdout.flush() 
-server.close() 
+
+def connect():
+
+    def reconnect():
+        # Destroy the chat window
+        chatWindow.destroy()
+
+    def on_closing(event=None):
+        if sendToServer:
+            """This function is to be called when the window is closed."""
+            # Send message to the server indication the client has quit
+            sendMessage.set("dsnfjnd")
+            send()
+            time.sleep(1)
+            clientSocket.close()
+        chatWindow.destroy()
+
+    def receive():
+        """Handles receiving of messages."""
+        while True:
+            try:
+                # Get the message from the server
+                recvMessage = clientSocket.recv(4096).decode("utf8")
+                # If the server responds with the closing message, write to the interface and break the loop
+                if recvMessage == "dsnfjnd":
+                    messageList.insert(END, "Server has disconnected!")
+                    break
+                messageList.insert(END, recvMessage)
+                messageList.yview_moveto(1)
+            # Client may have left the chat and handles closing the thread when the chat window exits
+            except OSError: 
+                break
+        
+
+    def send(event=None):  
+        # Get the message from the input field
+        msg = sendMessage.get()
+        # Clear the input field.
+        sendMessage.set("")  
+        try:
+            #  Send the message to the server
+            clientSocket.send(msg.encode())
+        except:
+            # Notify the user in the update frame
+            updateSendText.set("Message not sent. Connection may have lost")
+            reconnectButton.config(state="normal")
+
+    sendToServer = 1
+    # Lines 56 to 69 is just GUI stuff
+    chatWindow = Toplevel(root)
+    chatWindow.title("Chat Away!")
+    chatWindow.grab_set()
+    width = 800
+    height = 400
+    chatWindow.minsize(width=width, height=height)
+
+    updateText = StringVar()
+    updateShow = Label(chatWindow, textvariable=updateText, width="100", fg="red")
+    updateShow.pack()
+
+    updateSendText = StringVar()
+    updateSendShow = Label(chatWindow, textvariable=updateSendText, width="100", fg="red")
+    updateSendShow.pack()
+
+    messagesFrame = Frame(chatWindow)
+    # Holder for the entered message
+    sendMessage = StringVar()  
+    scrollbar = Scrollbar(messagesFrame) 
+    # List to contain the messages and display them to the user
+    messageList = Listbox(messagesFrame, height=15, width=120, yscrollcommand=scrollbar.set)
+    scrollbar.pack(side=RIGHT, fill=Y)
+    messageList.pack(side=LEFT, fill=BOTH)
+    messageList.pack()
+    messagesFrame.pack()
+
+    inputFrame = Frame(chatWindow)
+    inputFrame.pack(pady=5)
+    entryField = Entry(inputFrame, textvariable=sendMessage, width = 65)
+    # Allows the user to send to the server by clicking the enter (return) key
+    entryField.bind("<Return>", send)
+    entryField.grid(column=0, row=0, padx=5)
+    send_button = Button(inputFrame, text="Send", command=send, relief=GROOVE)
+    send_button.grid(column=1, row=0)
+
+    reconnectButton = Button(chatWindow, text="Redirect to Home Page", command=reconnect, fg="green", state=DISABLED, relief=GROOVE)
+    reconnectButton.pack(pady=20)
+
+    chatWindow.protocol("WM_DELETE_WINDOW", on_closing)
+    ADDR = ("3.83.189.76", 3000)
+
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    try:
+        clientSocket.connect(ADDR)
+    except:
+        # Don't allow sending to the server
+        sendToServer = 0
+        updateText.set(
+            "Cannot connect to the server. Make sure it is running OR make sure the info you have entered is correct!")
+        send_button.config(state = "disabled" )
+        reconnectButton.config(state = "normal")
+    # Create thread
+    recvThread = Thread(target=receive)
+    recvThread.start()
+    
+    
+
+# Shut down the program
+def closeMain():
+    root.quit()
+
+# Define the GUI root and set the title
+root = Tk()
+root.title("Configure Connection")
+# Set the dimensions 
+width = 500
+height = 400
+root.minsize(width=width, height=height)
+
+welcomeFrame = Frame(root)
+welcomeLabel = Label(welcomeFrame, text="Welcome to the chat app!")
+welcomeLabel.pack()
+welcomeFrame.pack(pady=20)
+
+connectButton = Button(root, text="Connect", command=connect, relief=GROOVE, width="15", height="1", fg="green")
+connectButton.pack(pady=10)
+
+exitButton = Button(root, text="Exit", command=closeMain, relief=GROOVE, width="15", height="1", fg="red")
+exitButton.pack(pady=10)
+
+# Launch the GUI
+root.mainloop()
--- server.py
+++ server.py
@@ -1,23 +1,22 @@
 import socket 
-from threading import *
-
-
+import threading
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 
-#get ip for the server
-print("Enter server IP address: ")
-IP = str(input()) 
+IP='0.0.0.0'
+port=3000
 
-#get port number for the server
-print("Enter server port number: ")
-port = int(input()) 
+endFlag= False
 
+#enter number of chat rooms the server should support
 print("How many chat rooms would you like?")
 numRooms = int(input())
 
-#make a room list with a lists embedded in it to hold the clients in each room
+#list to hold all client threads so we can join them to the main thread when the server shuts down
+clientThreads = []
+
+#make a 2D room list where the rows are the different chat rooms and each spot in a row is a client connection
 roomList=[]
 for x in range(0, numRooms):
     roomList.append([])
@@ -29,83 +28,223 @@ server.bind((IP, port))
 #server listens for up to 100 connections
 server.listen(100)
 
+#set server socket timeout to 3 so it doesnt block forever when waiting to accept connections (this is so server thread can terminate gracegully)
+server.settimeout(3)
 
-clientConnections = []
-
-  
+#client thread that is created each time a client connects
 def clientthread(conn, addr): 
-  
+    
+    #set client connection timeout so when it blocks on recv() it will eventually timeout (this is so client thread can terminate gracegully)
+    conn.settimeout(3)
+    
+    room=""
+    #flag for main loop when client is chatting in the chatroom different from end flag so it only closes the current client
+    flag = True
+    
     #Let client know they are connected
     m="Connected successfully! What is your name?"
     conn.send(m.encode())
-    name = conn.recv(2048)
     
-    m = "There are currently "+len(roomList)+" rooms which room would you like to join?"
-    conn.send(m.encode())
-    room = int(conn.recv(2048))
-    roomList[room].append(conn)
-       
-    
-    while True: 
-            try: 
-                message = conn.recv(4096)
-                
-                if message: 
-  
-                    #prints the name and msg that was just sent to the server
-                    print("From: " + name +" Message:" + message) 
-  
-                    # Calls sendAll function to send a message to all clients in the room
-                    msg = "From: " + name +" Message: " + message 
-                    sendAll(msg.encode(), conn, room) 
-  
-                else: 
-                    #If the message has no content there may be a connection error so remove the client
-                    removeClient(conn, room) 
-  
-            except: 
+    #recv() will eventually timeout and cause an acception which will be caught and allow it to continue waiting for input
+    #but if the server has started to shutdown the endFlag will become true and will break the loop
+    name=""
+    while not(endFlag):
+            try:
+                #wait for client to send a message
+                name = conn.recv(2048).decode()
+            except:
+                #this string is sent by the client when it closes the connection from its side so the client thread can terminate on the server
+                if name=='dsnfjnd':
+                    flag=False
+                    break
+                #this is so when the client supplies a name it will break this loop and move on to the next part of the code
+                elif name!= "":
+                    break
+                #if client hasnt given a name and also hasnt closed the connection continue waiting for a message
                 continue
-  
+    
+    #this is so if the client has closed its connection or the server has started to shutdown before the room number is entered it will skip this section
+    if not(name=='dsnfjnd') and not(endFlag):
+        
+        #ask which chat room the client would like to join
+        m = "Hello " +name+ "! There are currently "+str(len(roomList))+" rooms which room would you like to join?"
+        conn.send(m.encode())
+        
+        #same logic as first recv loop
+        room=""
+        while not(endFlag):
+                try:
+                    room = conn.recv(2048).decode()
+                except:
+                    if room=='dsnfjnd':
+                        flag=False
+                        break
+                    elif room!= "":
+                        break
+                    continue
+        
+        #make sure a valid room number is entered
+        while(not(isNumber(room)) or int(room)>numRooms or int(room)<0):
+            
+            #if the server is shutting down or client has exited break this loop and set flag for main receiving loop to false
+            if endFlag or room =='dsnfjnd':
+                flag=False
+                break
+            
+            #give them a chance to enter a valid room number
+            m="Error you did not enter a number or the number is not valid, Please enter a valid room number: "
+            conn.send(m.encode())
+            
+            room=""
+            while not(endFlag):
+                    try:
+                        room = conn.recv(2048).decode()
+                    except:
+                        if room=='dsnfjnd':
+                            flag=False
+                            break
+                        elif room!= "":
+                            break
+                        continue
 
-#sends a message to all clients in the chat except yourself
+        #if room is a number add client connection to room of their choice
+        if isNumber(room):
+            room = int(room)
+            roomList[room-1].append(conn)
+            
+            #tell client they are connected
+            m="You are now connected to chat room "+str(room)+"!"
+            conn.send(m.encode())
+    
+    #if the client has exited at any point dont enter main message receiving loop  
+    if name=='dsnfjnd' or room=='dsnfjnd':
+        flag=False
+
+    conn.settimeout(5)
+    
+    #main while loop for receiving client messages and sending them to everyone in the chat room
+    while flag:
+        
+        #check if server is shutting down
+        if endFlag:
+            break
+        
+        try: 
+            message = conn.recv(4096).decode()
+            
+            #if the client has disconnected break loop
+            if message=='dsnfjnd':
+                msg= name+' has left the chat!'
+                sendAll(msg.encode(), conn, room)
+                removeClient(conn, room)
+                break
+            
+            #if a message has been received from client send it to everyone in the chat room
+            if message: 
+                #prints the name and msg that was just sent to the server
+                print("From: " + name +" Message:" + message) 
+                
+                # Calls sendAll function to send a message to all clients in the room
+                msg = "From: " + name +" Message: " + message 
+                sendAll(msg.encode(), conn, room) 
+            else:
+                #If the message has no content there may be a connection error so remove the client
+                removeClient(conn, room)
+                break
+                
+        except:
+            continue
+    
+    #remove client from room because thread is exiting          
+    if not(room=="") and isNumber(room):
+        removeClient(conn, room)
+    
+    #if server is shutting down tell client so it can close properly 
+    if endFlag:
+        m="dsnfjnd"
+        conn.send(m.encode())
+    
+    #close the connection  
+    conn.close()
+    print(addr[0]+" has disconnected!")
+    
+
+#sends a message to all clients in the chat
 def sendAll(msg, connection, roomNum): 
-    for client in roomList[roomNum]: 
-        if client!=connection: 
-            try: 
-                client.send(msg) 
-            except: 
-                client.close() 
-  
-                # if the link is broken, we remove the client 
-                removeClient(client) 
+    for client in roomList[roomNum-1]: 
+        try: 
+            client.send(msg) 
+        except:
+            client.close()
+            # if the link is broken, we remove the client 
+            removeClient(client, roomNum) 
+            
   
 
-#removes a client from the client list if the connection is closed
-def removeClient(connection, room): 
-    if connection in roomList[room]: 
-        roomList[room].remove(connection) 
+#removes a client from the room list if the connection is closed
+def removeClient(connection, room):
+    if connection in roomList[room-1]: 
+        roomList[room-1].remove(connection)
+        
+#check if a value is a number      
+def isNumber(s):
+    try:
+        #attempt cast to int if it throws an exception return false otherwise return true
+        int(s)
+        return True
+    except ValueError:
+        return False
+        
+#server control thread that waits for incoming client connections 
+def serverThread():
+    
+    #while the end flag is false keep waiting for client connections
+    while not(endFlag): 
+    
+        try: 
+            #accepts a connection request and sores the client socket object and ip address
+            #timeout was set for accept() blocking call so that if the server shuts down it can exit
+            #when timeout happens an exception is thrown which is caught
+            conn, addr = server.accept()
+            
+            # prints the address of the user that just connected 
+            print (addr[0] + " connected")
+            
+            # creates and individual client thread for every user that connects 
+            thread = threading.Thread(target=clientthread, args=(conn,addr))
+            thread.start()
+            
+            #adds new client thread to the list
+            clientThreads.append(thread)
+            
+        except:
+            continue
 
+#start server control thread
+serverT = threading.Thread(target=serverThread)
+serverT.start()
 
-while True: 
+print("Server up and running on IP " +IP+ " and port "+str(port) +"\n")
 
-    #accepts a connection request and sores the client socket object and ip address
-    conn, addr = server.accept() 
-  
-    #adds new client connection to the client list
-    clientConnections.append(conn) 
-  
-    # prints the address of the user that just connected 
-    print (addr[0] + " connected")
-  
-    # creates and individual thread for every user  
-    # that connects 
-    thread = Thread(clientthread(conn, addr))
-    thread.start()   
+#wait for enter to be pressed to close the server
+input("Press Enter to close server...\n")
 
- 
-conn.close() 
+#set end flag so client threads can break while loop and close properly
+endFlag=True
+
+#join any client threads that havent exited yet to the main thread so they can finish their execution before main thread exits
+for client in clientThreads:
+    client.join()
+    print(str(client.getName())+" closed")
+
+#wait for server thread to finish if it hasnt already
+if serverT.is_alive():
+    serverT.join()
+
+#close server socket
 server.close()
 
+print('Server closed gracefully')
 
 
 
